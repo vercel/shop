09febab587328873deeeb5bfc82efced3e8a4013
Dan Laugharn <laugharn@gmail.com>
2026-04-15 08:45:17 -0500

    Observes

--- apps/template/next.config.ts
+++ apps/template/next.config.ts
@@ -4,7 +4,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 const nextConfig: NextConfig = {
   cacheComponents: true,
   experimental: {
-    cachedNavigations: true,
+    // cachedNavigations: true,
     inlineCss: true,
     optimisticRouting: true,
     partialFallbacks: true,
