97717284c88ef7dfe91604b505c70fba9b5b2fd3
Dan Laugharn <laugharn@gmail.com>
2026-04-18 10:39:26 -0500

    Removes varyParams

--- apps/template/next.config.ts
+++ apps/template/next.config.ts
@@ -9,7 +9,6 @@ const nextConfig: NextConfig = {
     optimisticRouting: true,
     partialFallbacks: true,
     turbopackFileSystemCacheForDev: true,
-    // varyParams: true,
   },
   images: {
     minimumCacheTTL: 31536000,
